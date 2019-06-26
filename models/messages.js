5943c556cda916c31b1c05f40f74f28ca79f77a8
shirleywbi <swwbi@msn.com>
2019-06-25 18:28:06 -0700

    start working on mongodb connection

---
+++ models/messages.js
@@ -0,0 +1,40 @@
+import { MongoClient } from 'mongodb';
+
+const URL = 'mongodb://localhost:9000/messages';
+
+let messages = [
+    {
+        name: 'Rick',
+        text: 'We\'re no strangers to love. You know the rules and so do I. ' +
+        'A full commitment\'s what I\'m thinking of. You wouldn\'t get this from any other guy.',
+        date: "6/2/2019 11:45:11",
+        id: "622019114511"
+    },
+    {
+        name: 'Richard',
+        text: 'I just wanna tell you how I\'m feeling... Gotta make you understand...',
+        date: "6/2/2019 11:45:30",
+        id: "622019114530"
+    },
+    {
+        name: 'Paul',
+        text: 'Never gonna give you up. Never gonna let you down. ' +
+        'Never gonna run around and desert you. Never gonna make you cry. ' +
+        'Never gonna say goodbye.',
+        date: "6/2/2019 11:50:00",
+        id: "622019115000"
+    },
+    {
+        name: 'Rick Astley',
+        text: 'Never gonna tell a lie and hurt you.',
+        date: "6/3/2019 9:45:04",
+        id: "63201994504"
+    }];
+
+MongoClient.connect(URL, function(err, db) {
+    if (err) return;
+    let collection = db.collection('messages');
+    collection.insertMany(messages);
+    console.log(db.messages.find().pretty());
+    db.close;
+})
